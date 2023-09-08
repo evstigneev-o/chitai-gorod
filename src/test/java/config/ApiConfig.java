e4056d3f43b0f32fdb5647f74860766cf3562ce3
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2023-09-08 14:45:19 +0300

    add web and api tests

--- src/test/java/config/ApiConfig.java
+++ src/test/java/config/ApiConfig.java
@@ -2,6 +2,13 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources(
+        {
+                "classpath:${env}.properties"
+        }
+)
+
 public interface ApiConfig extends Config {
     @Key("baseApiUrl")
     @DefaultValue("https://web-gate.chitai-gorod.ru/api/v1")
--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -2,6 +2,13 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources(
+        {
+                "classpath:${env}.properties"
+        }
+)
+
 public interface WebConfig extends Config {
     @Key("browser")
     @DefaultValue("chrome")
